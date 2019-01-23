b7d85162d768e9a461ac1fd196a26ea420add248
zuochunsheng <zuochunsheng@ledcas.com>
2019-01-23 16:28:23 +0800

    首次提交 stomp 链接 订阅发送 （http /wss ）

---
+++ app/src/main/java/com/android/myapplicationtest/util/Config.java
@@ -0,0 +1,24 @@
+package com.android.myapplicationtest.util;
+
+/**
+ * anther: created by zuochunsheng on 2019/1/22 14 : 58
+ * description :
+ * https://ops.ledcas.com/wsconnect/
+ */
+public class Config {
+    //http://192.168.1.47:8082/oss-transaction/messages
+   // public static final String WS_URI = "https://ops.ledcas.com/oss-transaction/messages";
+
+    //public static final String WS_URI = "https://ops.ledcas.com/oss-transaction/messages/websocket";
+
+    //public static final String WS_URI = "wss://ops.ledcas.com/oss-transaction/messages";
+    //public static final String WS_URI = "wss://ops.ledcas.com/oss-transaction/messages/websocket";
+
+
+
+    //public static final String WS_URI = "wss://ops.ledcas.com/wsconnect/websocket";
+    public static final String WS_URI = "https://ops.ledcas.com/wsconnect/websocket";
+
+
+    //public static final String VERSION = "accept-version";
+}
