f1c0d5d4b8cdf42ca2a558129bd23541a55443f6
zuochunsheng <zuochunsheng@ledcas.com>
2019-12-30 17:47:11 +0800

    自定义 转盘效果

--- app/src/main/java/com/android/myapplicationtest/activity/CustomActivity.java
+++ app/src/main/java/com/android/myapplicationtest/activity/CustomActivity.java
@@ -2,14 +2,46 @@ package com.android.myapplicationtest.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.android.myapplicationtest.R;
+import com.android.myapplicationtest.view.zhuanpan.LuckyPanView;
+
+import java.util.Random;
 
 public class CustomActivity extends Activity {
+
+    private LuckyPanView mLuckyPanView;
+    private ImageView mStartBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom);
+
+        mLuckyPanView = (LuckyPanView) findViewById(R.id.id_luckypan);
+        mStartBtn = (ImageView) findViewById(R.id.id_start_btn);
+
+        mStartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mLuckyPanView.isStart()) {
+                    mStartBtn.setImageResource(R.drawable.ic_notifications_none_black_24dp);
+                    mLuckyPanView.luckyStart(1);
+
+                    //Random random=new Random();
+                    //mLuckyPanView.luckyStart(random.nextInt()%6);
+
+                } else {
+                    if (!mLuckyPanView.isShouldEnd()) {
+                        mStartBtn.setImageResource(R.mipmap.note);
+                        mLuckyPanView.luckyEnd();
+                    }
+                }
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/android/myapplicationtest/view/zhuanpan/SurfaceViewTemplate.java
@@ -0,0 +1,100 @@
+package com.android.myapplicationtest.view.zhuanpan;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * @author： zcs
+ * @time：2019/12/30 on 11:53
+ * @description：https://blog.csdn.net/lmj623565791/article/details/41722441
+ *
+ * 一般SurfaceView类中我们会这么写代码：
+ */
+public class SurfaceViewTemplate extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+    private SurfaceHolder mHolder;
+    /**
+     * 与SurfaceHolder绑定的Canvas
+     */
+    private Canvas mCanvas;
+    /**
+     * 用于绘制的线程
+     */
+    private Thread t;
+    /**
+     * 线程的控制开关
+     */
+    private boolean isRunning;
+
+    public SurfaceViewTemplate(Context context) {
+        this(context, null);
+    }
+
+    public SurfaceViewTemplate(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+
+        // setZOrderOnTop(true);// 设置画布 背景透明
+        // mHolder.setFormat(PixelFormat.TRANSLUCENT);
+
+        //设置可获得焦点
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        //设置常亮
+        this.setKeepScreenOn(true);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        // 开启线程
+        isRunning = true;
+        t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // 通知关闭线程
+        isRunning = false;
+    }
+
+    @Override
+    public void run() {
+        // 不断的进行draw
+        while (isRunning) {
+            draw();
+        }
+
+    }
+
+    private void draw() {
+        try {
+            // 获得canvas
+            mCanvas = mHolder.lockCanvas();
+            if (mCanvas != null) {
+                // drawSomething..
+            }
+        } catch (Exception e) {
+        } finally {
+            if (mCanvas != null) {
+                mHolder.unlockCanvasAndPost(mCanvas);
+            }
+        }
+    }
+
+
+}
